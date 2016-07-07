6351faede9304b9f9e15d22dd34330eb9a249cb8
twmb <travis.bischel@gmail.com>
2016-07-06 18:33:58 -0700

    add spscdvq based off dvyukov's mpmc queue

---
+++ queue/spsc/spscdvq/dvq.go
@@ -0,0 +1,41 @@
+package spscdvq
+
+import (
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/twmb/dash/primitive"
+)
+
+// See mpmc's mpmcdvq for full comments. This code is that mpmc, whittled down
+// assuming there is max one enqueue concurrent with one dequeue.
+
+// TryEnqueue adds a value to our queue. TryEnqueue takes an unsafe.Pointer to
+// avoid the necessity of wrapping a heap allocated value in an interface,
+// which also goes on the heap. If the queue is full, this will return failure.
+func (q *Queue) TryEnqueue(ptr unsafe.Pointer) (enqueued bool) {
+	c := (*cell)(unsafe.Pointer(uintptr(q.bufPtr) + (cellSz * (q.enqPos & q.mask))))
+	seq := atomic.LoadUintptr(&c.seq)
+	if seq < q.enqPos {
+		return
+	}
+	q.enqPos++
+	c.ptr = ptr
+	atomic.StoreUintptr(&c.seq, q.enqPos)
+	return true
+}
+
+// TryDequeue dequeues a value from our queue. If the queue is empty, this
+// will return failure.
+func (q *Queue) TryDequeue() (ptr unsafe.Pointer, dequeued bool) {
+	c := (*cell)(unsafe.Pointer(uintptr(q.bufPtr) + (cellSz * (q.deqPos & q.mask))))
+	seq := atomic.LoadUintptr(&c.seq)
+	if seq < q.deqPos+1 {
+		return
+	}
+	q.deqPos++
+	ptr = c.ptr
+	c.ptr = primitive.Null
+	atomic.StoreUintptr(&c.seq, q.deqPos+q.mask)
+	return ptr, true
+}
